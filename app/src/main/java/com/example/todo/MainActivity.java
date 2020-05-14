9e2a45805adcf25e1d0230e2a605d3b57f828880
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-14 19:41:15 +0900

    added another page and menu

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.todo;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> notes = new ArrayList<String>();
+    static ArrayList<String> notes = new ArrayList<String>();
+    static ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView  listView = (ListView) findViewById(R.id.listView);
+        notes.add("example note");
+        notes.add("hihihi");
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), notedetail.class);
+                intent.putExtra("noteId",position);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    // MENU
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        
+        return super.onOptionsItemSelected(item);
     }
 }
